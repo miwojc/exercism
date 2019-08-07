564b0bd2b8adb58bc939d2d723ab37bf1ed7cf48
miwojc <miwojc@protonmail.com>
2019-08-07 09:50:50 +0200

    Solved pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,8 @@
-def is_pangram(sentence):
-    pass
+def is_pangram(sentence: str) -> bool:
+    """Returns `True` if a sentence is a pangram, otherwise returns `False`"""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for letter in alphabet:
+        test = letter in sentence.lower()
+        if not test:
+            return test
+    return test
