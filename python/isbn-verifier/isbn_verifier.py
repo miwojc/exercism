7d51a335ac153ec3925e4426820cc6108e6f8470
miwojc <miwojc@protonmail.com>
2019-08-09 20:44:05 +0200

    Refactored isbn_verifier.py

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,34 +1,9 @@
+import re
 def is_valid(isbn: str) -> bool:
     """Returns `True` if ISBN-10 is valid, otherwise returns `False`"""
-
-    val = 0
-    multi = 10
     isbn = isbn.replace("-", "")
-
-    # check if input string is 10 digits including a check character
-    if len(isbn) != 10:
-        return False
-
-    # check if isbn contains valid characters only
-    if not isbn[0:9].isdigit():
-        return False
-    
-    if not isbn[-1:].isdigit():
-        if not isbn[-1:] == "X":
-            return False
-    
-    isbn = isbn.replace("X", "10")
-
-    # check if isbn is valid
-    if len(isbn) > 10:
-        for digit in isbn[0:9]:
-            val += int(digit) * multi
-            multi -= 1
-        val = val + 10
-        return val % 11 == 0
-
-    for digit in isbn:
-        val += int(digit) * multi
-        multi -= 1
-
-    return val % 11 == 0
+    if re.match("\\d{9}[\\dX]$", isbn):
+        digits = [int(s) if s != "X" else 10 for s in isbn]
+        formula = sum(map(lambda a, b: a * b, digits, range(10, 0, -1)))
+        return formula % 11 == 0
+    return False
