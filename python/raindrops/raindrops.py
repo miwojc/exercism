d2f8cda66a1ed02ef2fce306c4481f2083c67a58
miwojc <miwojc@protonmail.com>
2019-08-26 10:37:23 +0200

    Solved raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,12 @@
-def convert(number):
-    pass
+def convert(number: int) -> str:
+    """Converts a number into string that contains raindrop sounds"""
+    if number % 3 and number % 5 and number % 7 != 0:
+        return str(number)
+    result = ""
+    if number % 3 == 0:
+        result += "Pling"
+    if number % 5 == 0:
+        result += "Plang"
+    if number % 7 == 0:
+        result += "Plong"
+    return result
