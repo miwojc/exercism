da6267bbf21928a9c52007b21e8fbc2dc45b8f95
miwojc <miwojc@protonmail.com>
2019-08-30 19:36:06 +0200

    Refactored raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,12 +1,12 @@
 def convert(number: int) -> str:
     """Converts a number into a string that contains raindrop sounds"""
-    result = ""
+    sounds = ""
     if number % 3 == 0:
-        result += "Pling"
+        sounds += "Pling"
     if number % 5 == 0:
-        result += "Plang"
+        sounds += "Plang"
     if number % 7 == 0:
-        result += "Plong"
-    if number % 3 and number % 5 and number % 7 != 0:
-        result += str(number)
-    return result
+        sounds += "Plong"
+    if sounds == "":
+        sounds += str(number)
+    return sounds
