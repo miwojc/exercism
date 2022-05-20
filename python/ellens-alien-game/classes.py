817c992cc6589193572d8cf1cd649986c878f81d
miwojc <miwojc@protonmail.com>
2022-05-20 07:39:02 +0200

    solve ellens alien game

--- python/ellens-alien-game/classes.py
+++ python/ellens-alien-game/classes.py
@@ -19,4 +19,27 @@ class Alien:
     collision_detection(other): Implementation TBD.
     """
 
-    pass
+    total_aliens_created = 0
+
+    def __init__(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.health = 3
+        Alien.total_aliens_created += 1
+
+    def hit(self):
+        self.health -= 1
+
+    def is_alive(self):
+        return True if self.health > 0 else False
+
+    def teleport(self, new_x_coordinate, new_y_coordinate):
+        self.x_coordinate = new_x_coordinate
+        self.y_coordinate = new_y_coordinate
+
+    def collision_detection(self, other):
+        pass
+
+
+def new_aliens_collection(alien_start_positions):
+    return [Alien(coord[0], coord[1]) for coord in alien_start_positions]
