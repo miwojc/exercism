63ed6bdbf56ec2333d960282a8b3c073e148bc5c
miwojc <miwojc@protonmail.com>
2022-05-22 07:09:11 +0200

    solve scrabble score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,20 @@
 def score(word):
-    pass
+    def score_word(word):
+        if word in ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"):
+            return 1
+        elif word in ("D", "G"):
+            return 2
+        elif word in ("B", "C", "M", "P"):
+            return 3
+        elif word in ("F", "H", "V", "W", "Y"):
+            return 4
+        elif word in ("K"):
+            return 5
+        elif word in ("J", "X"):
+            return 8
+        elif word in ("Q", "Z"):
+            return 10
+        else:
+            return 0
+
+    return sum((score_word(w) for w in word.upper()))
