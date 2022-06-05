26576ffbe59d10015f92b9cc8d5b9bc6c799c5c6
miwojc <miwojc@protonmail.com>
2022-06-05 07:19:03 +0200

    solve grains

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,9 @@
 def square(number):
-    pass
+    if 0 < number < 65:
+        return 2 ** (number - 1)
+    else:
+        raise ValueError("square must be between 1 and 64")
 
 
 def total():
-    pass
+    return sum((2**i for i in range(64)))
