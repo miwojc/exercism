a58d78338a4c509259ff5f6530c2e6eaaeccb06d
miwojc <miwojc@protonmail.com>
2022-06-09 13:50:52 +0200

    solve darts

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,9 @@
 def score(x, y):
-    pass
+    dist = ((x**2 + y**2))**(1/2)
+    if dist > 10:
+        return 0
+    if 5 < dist <= 10:
+        return 1
+    if 1 < dist <= 5:
+        return 5
+    return 10
