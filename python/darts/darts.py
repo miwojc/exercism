35aa2dca26056f7f18dc8d700bf27032b9107409
miwojc <miwojc@protonmail.com>
2022-06-09 14:00:26 +0200

    refactor darts

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,9 +1,9 @@
 def score(x, y):
-    dist = ((x**2 + y**2))**(1/2)
-    if dist > 10:
-        return 0
-    if 5 < dist <= 10:
-        return 1
-    if 1 < dist <= 5:
+    dist = ((x**2 + y**2)) ** (1 / 2)
+    if dist <= 1:
+        return 10
+    if dist <= 5:
         return 5
-    return 10
+    if dist <= 10:
+        return 1
+    return 0
