7ad5ee723d44c207a2065bdc49b7983462d69914
miwojc <miwojc@protonmail.com>
2022-06-10 13:03:29 +0200

    solve sum of multiples

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,7 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    total = set()
+    for number in multiples:
+        if number > 0:
+            for i in range(number, limit, number):
+                total.add(i)
+    return sum(total)
