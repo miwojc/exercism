f088bdca9c2797ad8b8b0be5bce41eb89118737a
miwojc <miwojc@protonmail.com>
2022-06-11 12:59:56 +0200

    solve anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,9 @@
 def find_anagrams(word, candidates):
-    pass
+    word_lower = word.lower()
+    word_sorted = sorted(word_lower)
+    _candidates = [[candidate, candidate.lower()] for candidate in candidates]
+    return [
+        candidate[0]
+        for candidate in _candidates
+        if sorted(candidate[1]) == word_sorted and candidate[1] != word_lower
+    ]
