5b5d935b259e862347feccfd61915b0f2b3c2ad6
miwojc <miwojc@protonmail.com>
2022-06-12 07:30:32 +0200

    solve difference of squares

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,10 @@
 def square_of_sum(number):
-    pass
+    return sum((number for number in range(1, number + 1))) ** 2
 
 
 def sum_of_squares(number):
-    pass
+    return sum(number**2 for number in range(1, number + 1))
 
 
 def difference_of_squares(number):
-    pass
+    return square_of_sum(number) - sum_of_squares(number)
