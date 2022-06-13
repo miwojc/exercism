3a9beefe23adde6adcdfb024b76eedf1edb45812
miwojc <miwojc@protonmail.com>
2022-06-13 14:49:07 +0200

    solve flatten array

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,9 @@
 def flatten(iterable):
-    pass
+    flattened = []
+    for item in iterable:
+        if item or item == 0:
+            if isinstance(item, list):
+                flattened += flatten(item)
+            else:
+                flattened.append(item)
+    return flattened
