a1c50346b3bcc16b29c712c278a1b6f8675deb43
miwojc <miwojc@protonmail.com>
2022-06-14 06:43:30 +0200

    solve perfect numbers

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,7 +1,15 @@
 def classify(number):
-    """ A perfect number equals the sum of its positive divisors.
+    """A perfect number equals the sum of its positive divisors.
 
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number <= 0:
+        raise ValueError("Classification is only possible for positive integers.")
+    asum = sum((num for num in range(1, number) if not number % num))
+    if asum == number:
+        return "perfect"
+    if asum > number:
+        return "abundant"
+    if asum < number:
+        return "deficient"
