6e568a0636401100a979cff4726e26912f13a90f
miwojc <miwojc@protonmail.com>
2022-06-14 06:53:11 +0200

    refactor perfect numbers

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,12 +1,21 @@
+def aliquot_sum(number):
+    """Returns the sum of its positive divisors.
+
+    :param number: int a positive integer
+    :return: int sum of input number positive divisors
+    """
+    if number <= 0:
+        raise ValueError("Classification is only possible for positive integers.")
+    return sum((num for num in range(1, number // 2 + 1) if not number % num))
+
+
 def classify(number):
     """A perfect number equals the sum of its positive divisors.
 
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    if number <= 0:
-        raise ValueError("Classification is only possible for positive integers.")
-    asum = sum((num for num in range(1, number) if not number % num))
+    asum = aliquot_sum(number)
     if asum == number:
         return "perfect"
     if asum > number:
