eac77f753f8b2f35642a177d79c8f7a3eb3753dd
miwojc <miwojc@protonmail.com>
2022-06-16 08:15:37 +0200

    refactor space age

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,5 +1,15 @@
 class SpaceAge:
     seconds_earth = 31557600
+    ratios = {
+        "mercury": 0.2408467,
+        "venus": 0.61519726,
+        "earth": 1.0,
+        "mars": 1.8808158,
+        "jupiter": 11.862615,
+        "saturn": 29.447498,
+        "uranus": 84.016846,
+        "neptune": 164.79132,
+    }
 
     def __init__(self, seconds):
         self.seconds = seconds
@@ -8,25 +18,25 @@ class SpaceAge:
         return round(self.seconds / SpaceAge.seconds_earth / ratio, 2)
 
     def on_mercury(self):
-        return self.earth_years_old(0.2408467)
+        return self.earth_years_old(SpaceAge.ratios["mercury"])
 
     def on_venus(self):
-        return self.earth_years_old(0.61519726)
+        return self.earth_years_old(SpaceAge.ratios["venus"])
 
     def on_earth(self):
-        return self.earth_years_old(1)
+        return self.earth_years_old(SpaceAge.ratios["earth"])
 
     def on_mars(self):
-        return self.earth_years_old(1.8808158)
+        return self.earth_years_old(SpaceAge.ratios["mars"])
 
     def on_jupiter(self):
-        return self.earth_years_old(11.862615)
+        return self.earth_years_old(SpaceAge.ratios["jupiter"])
 
     def on_saturn(self):
-        return self.earth_years_old(29.447498)
+        return self.earth_years_old(SpaceAge.ratios["saturn"])
 
     def on_uranus(self):
-        return self.earth_years_old(84.016846)
+        return self.earth_years_old(SpaceAge.ratios["uranus"])
 
     def on_neptune(self):
-        return self.earth_years_old(164.79132)
+        return self.earth_years_old(SpaceAge.ratios["neptune"])
