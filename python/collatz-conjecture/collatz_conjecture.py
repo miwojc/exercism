9a5679a42954c4bd74437299e6c2de445cbfba23
miwojc <miwojc@protonmail.com>
2022-06-16 08:04:25 +0200

    refactor collatz conjecture

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,12 +1,8 @@
 def steps(number):
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
-    out = []
+    steps = 0
     while number != 1:
-        if number % 2:  # odd
-            number = number * 3 + 1
-            out.append(number)
-        else:  # even
-            number /= 2
-            out.append(number)
-    return len(out)
+        number = number * 3 + 1 if number % 2 else number / 2
+        steps += 1
+    return steps
