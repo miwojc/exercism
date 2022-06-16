92052dd0e8fe9b905f2aae91d8c5e9f37726344b
miwojc <miwojc@protonmail.com>
2022-06-16 07:54:34 +0200

    solve collatz conjecture

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,12 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    out = []
+    while number != 1:
+        if number % 2:  # odd
+            number = number * 3 + 1
+            out.append(number)
+        else:  # even
+            number /= 2
+            out.append(number)
+    return len(out)
