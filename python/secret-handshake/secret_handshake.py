5636ef400365b9bd9d7d0423e0cf10bb9d118dbc
miwojc <miwojc@protonmail.com>
2022-06-17 07:51:03 +0200

    solve secret handshake

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,2 +1,11 @@
 def commands(binary_str):
-    pass
+    out = []
+    if int(binary_str[-1]):
+        out.append("wink")
+    if int(binary_str[-2]):
+        out.append("double blink")
+    if int(binary_str[-3]):
+        out.append("close your eyes")
+    if int(binary_str[-4]):
+        out.append("jump")
+    return out[::-1] if int(binary_str[-5]) else out
