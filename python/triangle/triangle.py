d9a5a532aec8f7294df7ddb83cbe093f8b712f2d
miwojc <miwojc@protonmail.com>
2022-06-21 08:07:04 +0200

    solve triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,17 @@
 def equilateral(sides):
-    pass
+    a, b, c = sides
+    return (a == b == c) and is_triangle(a, b, c)
 
 
 def isosceles(sides):
-    pass
+    a, b, c = sides
+    return (a == b or a == c or b == c) and is_triangle(a, b, c)
 
 
 def scalene(sides):
-    pass
+    a, b, c = sides
+    return a != b and a != c and b != c and is_triangle(a, b, c)
+
+
+def is_triangle(a, b, c):
+    return a + b >= c and b + c >= a and a + c >= b and a > 0 and b > 0 and c > 0
