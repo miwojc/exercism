0e5cbc2a5263202dfba0885e65d4b4a24bbb1fe9
miwojc <miwojc@protonmail.com>
2022-07-07 18:29:19 +0200

    refactor rotational-cipher

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -3,11 +3,8 @@ from string import ascii_lowercase
 
 def rotate(text, key):
     ascii_lowercase_rot = ascii_lowercase[key:] + ascii_lowercase[:key]
-    out = [
-        ascii_lowercase_rot[ascii_lowercase.index(char)]
-        if char.lower() in ascii_lowercase
-        else char
-        for char in text.lower()
-    ]
-    out = [o.upper() if text[idx].isupper() else o for idx, o in enumerate(out)]
-    return "".join(out)
+    trans = str.maketrans(
+        ascii_lowercase + ascii_lowercase.upper(),
+        ascii_lowercase_rot + ascii_lowercase_rot.upper(),
+    )
+    return text.translate(trans)
