67ec40a8324622ed4cf606d40a73a352e00b5d39
miwojc <miwojc@protonmail.com>
2022-07-07 18:17:49 +0200

    solve rotational cipher

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,13 @@
+from string import ascii_lowercase
+
+
 def rotate(text, key):
-    pass
+    ascii_lowercase_rot = ascii_lowercase[key:] + ascii_lowercase[:key]
+    out = [
+        ascii_lowercase_rot[ascii_lowercase.index(char)]
+        if char.lower() in ascii_lowercase
+        else char
+        for char in text.lower()
+    ]
+    out = [o.upper() if text[idx].isupper() else o for idx, o in enumerate(out)]
+    return "".join(out)
