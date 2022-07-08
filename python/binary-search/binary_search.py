70334500357f46c33362798bdb181f2b6c0ff9e8
miwojc <miwojc@protonmail.com>
2022-07-08 08:16:39 +0200

    solve binary search

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,2 +1,10 @@
 def find(search_list, value):
-    pass
+    if not search_list:
+        raise ValueError("value not in array")
+    mid = len(search_list) // 2
+    if search_list[mid] == value:
+        return mid
+    elif value < search_list[mid]:
+        return find(search_list[:mid], value)
+    else:
+        return find(search_list[mid + 1 :], value) + mid + 1
