7cfe8390903b6bdaa6b5e6c6295b9bc1970fa390
miwojc <miwojc@protonmail.com>
2022-07-09 09:20:15 +0200

    solve list ops

--- python/list-ops/list_ops.py
+++ python/list-ops/list_ops.py
@@ -1,30 +1,47 @@
 def append(list1, list2):
-    pass
+    "`append` (*given two lists, add all items in the second list to the end of the first list*)"
+    return list1 + list2
 
 
 def concat(lists):
-    pass
+    "`concatenate` (*given a series of lists, combine all items in all lists into one flattened list*)"
+    return [el for lst in lists for el in lst]
 
 
 def filter(function, list):
-    pass
+    "`filter` (*given a predicate and a list, return the list of all items for which `predicate(item)` is True*)"
+    return [el for el in list if function(el)]
 
 
 def length(list):
-    pass
+    "`length` (*given a list, return the total number of items within it*)"
+    count = 0
+    for _ in list:
+        count += 1
+    return count
 
 
 def map(function, list):
-    pass
+    "`map` (*given a function and a list, return the list of the results of applying `function(item)` on all items*)"
+    return [function(el) for el in list]
 
 
 def foldl(function, list, initial):
-    pass
+    "`foldl` (*given a function, a list, and initial accumulator, fold (reduce) each item into the accumulator from the left using `function(accumulator, item)`*)"
+    accumulator = initial
+    for el in list:
+        accumulator = function(accumulator, el)
+    return accumulator
 
 
 def foldr(function, list, initial):
-    pass
+    "`foldr` (*given a function, a list, and an initial accumulator, fold (reduce) each item into the accumulator from the right using `function(item, accumulator)`*)"
+    accumulator = initial
+    for el in list[::-1]:
+        accumulator = function(el, accumulator)
+    return accumulator
 
 
 def reverse(list):
-    pass
+    "`reverse` (*given a list, return a list with all the original items, but in reversed order*)"
+    return list[::-1]
