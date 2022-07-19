990943b1239ada5208f4e06ae0898cb09f1509ae
miwojc <miwojc@protonmail.com>
2022-07-19 08:11:56 +0200

    solve pig latin

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,2 +1,23 @@
+# inspiration: https://exercism.org/tracks/python/exercises/pig-latin/solutions/tcarobruce
+
+VOWELS = "aeiouy"
+
+
+def _rotate(word):
+    return word[1:] + word[0]
+
+
+def _pig_latin(word):
+    if word[:2] == "xr":
+        return "xrayay"  # for some reason
+    if word[0] == "y" and word[1] in "aeiou":
+        word = _rotate(word)
+    while word[0] not in VOWELS:
+        word = _rotate(word)
+    if word[-1] == "q" and word[0] == "u":
+        word = _rotate(word)
+    return word + "ay"
+
+
 def translate(text):
-    pass
+    return " ".join([_pig_latin(word) for word in text.split()])
