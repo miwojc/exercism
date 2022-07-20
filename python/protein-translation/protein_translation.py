229ac999999e5c744c4f690ea3c921b8da84a8e3
miwojc <miwojc@protonmail.com>
2022-07-20 09:04:44 +0200

    solve protein translation

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,21 @@
 def proteins(strand):
-    pass
+    out = []
+    for idx in range(0, len(strand), 3):
+        condon = strand[idx : idx + 3]
+        if condon in ["UAA", "UAG", "UGA"]:
+            break
+        if condon == "AUG":
+            out.append("Methionine")
+        if condon in ["UUU", "UUC"]:
+            out.append("Phenylalanine")
+        if condon in ["UUA", "UUG"]:
+            out.append("Leucine")
+        if condon in ["UCU", "UCC", "UCA", "UCG"]:
+            out.append("Serine")
+        if condon in ["UAU", "UAC"]:
+            out.append("Tyrosine")
+        if condon in ["UGU", "UGC"]:
+            out.append("Cysteine")
+        if condon in ["UGG"]:
+            out.append("Tryptophan")
+    return out
