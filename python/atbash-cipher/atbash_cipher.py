eecf77a8171624e1dc7b610eaca2a745269d362f
miwojc <miwojc@protonmail.com>
2022-07-22 08:44:33 +0200

    solve atbash cipher

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,28 @@
+from string import ascii_lowercase, punctuation
+
+
 def encode(plain_text):
-    pass
+    out = []
+    for count, char in enumerate(
+        plain_text.replace(" ", "").translate(str.maketrans("", "", punctuation))
+    ):
+        char = char.lower()
+        if count % 5 == 0 and count != 0:
+            out.append(" ")
+        if char in ascii_lowercase and char not in punctuation:
+            idx = ascii_lowercase.index(char.lower())
+            out.append(ascii_lowercase[-(idx + 1)])
+        elif char not in punctuation:
+            out.append(char)
+    return "".join(out)
 
 
 def decode(ciphered_text):
-    pass
+    out = []
+    for char in ciphered_text.replace(" ", ""):
+        if char in ascii_lowercase:
+            idx = ascii_lowercase.index(char)
+            out.append(ascii_lowercase[-idx - 1])
+        else:
+            out.append(char)
+    return "".join(out)
