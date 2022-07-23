791bd2de8e34d6c9bcd13a04549bcc7d9f1f8198
miwojc <miwojc@protonmail.com>
2022-07-23 09:16:29 +0200

    solve yacht

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,18 +1,49 @@
 # Score categories.
-# Change the values as you see fit.
-YACHT = None
-ONES = None
-TWOS = None
-THREES = None
-FOURS = None
-FIVES = None
-SIXES = None
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+YACHT = 0
+ONES = 1
+TWOS = 2
+THREES = 3
+FOURS = 4
+FIVES = 5
+SIXES = 6
+FULL_HOUSE = 7
+FOUR_OF_A_KIND = 8
+LITTLE_STRAIGHT = 9
+BIG_STRAIGHT = 10
+CHOICE = 11
+
+from collections import Counter
 
 
 def score(dice, category):
-    pass
+    c = Counter(dice)
+    mc = c.most_common()
+    if 1 <= category <= 6:
+        return c[category] * category
+    if category == 7:
+        if len(c) == 2 and mc[0][1] == 3:
+            return mc[0][0] * mc[0][1] + mc[1][0] * mc[1][1]
+        else:
+            return 0
+    if category == 8:
+        if len(c) <= 2 and mc[0][1] >= 4:
+            return mc[0][0] * 4
+        else:
+            return 0
+    if category == 9:
+        if len(c) == 5 and (bool(c[1]) ^ bool(c[6])) and c[1]:
+            return 30
+        else:
+            return 0
+    if category == 10:
+        if len(c) == 5 and (bool(c[1]) ^ bool(c[6])) and c[6]:
+            return 30
+        else:
+            return 0
+    if category == 11:
+        return sum(dice)
+    if category == 0:
+        if len(c) == 1:
+            return 50
+        else:
+            return 0
