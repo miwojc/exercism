0acd313f1ecf9fd7872c2617717084404c6f6ead
miwojc <miwojc@protonmail.com>
2023-03-03 09:08:26 +0100

    rust solve assembly line

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {speed}")
+    speed as f64 * 221.0 * success_rate(speed)
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {speed}")
+    production_rate_per_hour(speed) as u32 / 60
+}
+
+fn success_rate(speed: u8) -> f64 {
+    match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        9|10 => 0.77,
+        _ => 0.0,
+    }
 }
