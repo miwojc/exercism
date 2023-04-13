47b2bc1b6b8182f85d937afd71d934fe0a3521cc
miwojc <miwojc@protonmail.com>
2023-04-13 08:32:44 +0200

    julia solve nucleotide count

--- julia/nucleotide-count/nucleotide-count.jl
+++ julia/nucleotide-count/nucleotide-count.jl
@@ -7,5 +7,11 @@ Invalid strands raise a `DomainError`.
 
 """
 function count_nucleotides(strand)
-
+    if !all(x -> x in "ACGT", strand)
+        throw(DomainError("Invalid strand"))
+    end
+    return Dict('A' => count(x -> x == 'A', strand),
+                'C' => count(x -> x == 'C', strand),
+                'G' => count(x -> x == 'G', strand),
+                'T' => count(x -> x == 'T', strand))
 end
