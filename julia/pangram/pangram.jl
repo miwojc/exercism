60b6f76c86e530abd49d6d99ea04fe22d4200c79
miwojc <miwojc@protonmail.com>
2023-04-15 08:37:37 +0200

    julia solve pangram

--- julia/pangram/pangram.jl
+++ julia/pangram/pangram.jl
@@ -5,6 +5,12 @@ Return `true` if `input` contains every alphabetic character (case insensitive).
 
 """
 function ispangram(input)
-
+    letters = Set{Char}()
+    for c in input
+        if isletter(c)
+            push!(letters, lowercase(c))
+        end
+    end
+    return length(letters) == 26
 end
 
