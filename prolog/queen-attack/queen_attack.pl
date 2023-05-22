7a6ecae1d39ef524dea470f03e8a834b43cfb531
miwojc <miwojc@protonmail.com>
2023-05-22 07:49:34 +0200

    prolog solve queen attack

--- prolog/queen-attack/queen_attack.pl
+++ prolog/queen-attack/queen_attack.pl
@@ -3,11 +3,13 @@
 % The create/1 predicate succeeds if the DimTuple contains valid chessboard 
 % dimensions, e.g. (0,0) or (2,4).
 create((DimX, DimY)) :-
-	fail.
+	maplist(between(0, 7), [DimX, DimY]).
 
 %! attack(+FromTuple, +ToTuple)
 %
 % The attack/2 predicate succeeds if a queen positioned on ToTuple is 
 % vulnerable to an attack by another queen positioned on FromTuple.
-attack((FromX, FromY), (ToX, ToY)):-
-	fail.
+attack((X, _), (X, _)) :- !.
+attack((_, Y), (_, Y)) :- !.
+attack((FromX, FromY), (ToX, ToY)) :-
+	abs(FromX - ToX) =:= abs(FromY - ToY).
