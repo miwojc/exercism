8f3486ebcfbcdc0bd6b76cd77c81cedda4760027
miwojc <miwojc@protonmail.com>
2023-05-27 07:22:13 +0200

    prolog triangle solve

--- prolog/triangle/triangle.pl
+++ prolog/triangle/triangle.pl
@@ -1,1 +1,9 @@
-triangle(Side1, Side2, Side3, Type).
+triangle(A, B, C, T) :-
+	A + B > C,
+	B + C > A,
+	C + A > B,
+	triangle_(A, B, C, T).
+
+triangle_(A, A, A, "equilateral") :- !.
+triangle_(A, B, C, "isosceles") :- (A =:= B; B =:= C; C =:= A), !.
+triangle_(A, B, C, "scalene") :- A =\= B, B =\= C, C =\= A, !.
